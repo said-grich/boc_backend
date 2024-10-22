ecb2b40c5c76d70791ddf13abf3348b94ed04360
said.grich <said.grich@outlook.com>
2024-10-22 01:23:41 +0100

    add Allowed hosts

--- boc_backEnd/settings.py
+++ boc_backEnd/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-foldj#@y^l0@rgd+bl0&5@0_3o&iiu0%k*0z4#q(nw%c2ou)lt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boc-backend-yntg.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
