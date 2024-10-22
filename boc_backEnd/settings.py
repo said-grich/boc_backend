b3f35514ec5bc951e736237d1a2ed6dc04e494a5
said.grich <said.grich@outlook.com>
2024-10-22 11:44:09 +0100

    Set STATIC_ROOT for Heroku

--- boc_backEnd/settings.py
+++ boc_backEnd/settings.py
@@ -199,7 +199,7 @@ SIMPLE_JWT = {
     'SIGNING_KEY': SECRET_KEY,
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
